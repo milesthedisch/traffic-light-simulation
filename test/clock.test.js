db8a2033503628a99955d1e9d2088a4178f8d5b2
Miles Disch <milesthedisch@gmail.com>
2017-05-20 12:31:03 +1000

    Add test for loop fn on clock class

--- test/clock.test.js
+++ test/clock.test.js
@@ -1,42 +1,58 @@
-const rafStub = require('raf-stub');
+const rafStub = require('raf-stub').replaceRaf();
+const now = require('performance-now');
 const assert = require('chai').assert;
 const clock = require('../lib/clock');
 
 
 describe("#clock", function() {
-  let clockInstance = Object.create(clock);
+  let clockInstance;
 
   beforeEach(function() {
-    clockInstance = Object.create(clock);
-  });
-
-  afterEach(function() {
-
+    clockInstance = Object.create(clock.init());
+    requestAnimationFrame.reset();
   });
 
   describe("init", function() {
     it("should return clock with initalized properties", function() {
-      clockInstance.init();
       assert.equal(clockInstance.callIndex, -1, 'The frameIndex is not -1');
     });
 
     it("should accept fps values as a param", function() {
       const fps = 60;
-
       clockInstance.init(fps);
       assert.equal(clockInstance.fps, fps);
     })
   });
 
   describe("start", function() {
-
+    it("should start the clock and save the start time", function() {
+      clockInstance.start();
+      assert.ok(clockInstance.startTime);
+    });
   });
 
   describe("stop", function() {
-
+    it("should stop the clock and save the stop time", function() {
+      clockInstance.stop();
+      assert.ok(clockInstance.stopTime);
+    });
   });
 
   describe("loop", function() {
+    it("should call the callback if enough time has passed", function(done) {
+      const then = now();
 
+      function callback(params) {
+        assert.equal(params.newTime, then, 'The new time was not then same as the controlled new time given');
+        assert.equal(params.clockStart, 0, 'Start time was not controlled');
+        assert.equal(params.callIndex, 0, 'callIndex was not increased by 1');
+        done();
+      }
+
+      clockInstance.fpms = 20;
+      clockInstance.startTime = 0;
+      clockInstance.lastTime = 0;
+      clockInstance.loop(then, callback);
+    });
   });
 });
